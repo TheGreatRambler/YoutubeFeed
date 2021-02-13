8633d27880470f4a9d7401de22b6413fca5d2668
TheGreatRambler <TheGreatRambler>
2021-02-13 02:34:43 -0500

    Add struct packing library

--- server/src/main.cpp
+++ server/src/main.cpp
@@ -1,24 +1,53 @@
-#include <iostream>
-
+#include <set>
 #include <websocketpp/config/asio_no_tls.hpp>
 #include <websocketpp/server.hpp>
 
 typedef websocketpp::server<websocketpp::config::asio> server;
 
-void on_message (websocketpp::connection_hdl, server::message_ptr msg) {
-	std::cout << msg->get_payload () << std::endl;
-}
+using websocketpp::connection_hdl;
+using websocketpp::lib::bind;
+
+class websocket_server {
+public:
+	websocket_server () {
+		m_server.init_asio ();
+
+		m_server.set_access_channels (websocketpp::log::alevel::all);
+		m_server.set_error_channels (websocketpp::log::elevel::all);
+
+		m_server.set_open_handler (bind (&websocket_server::on_open, this, websocketpp::lib::placeholders::_1));
+		m_server.set_close_handler (bind (&websocket_server::on_close, this, websocketpp::lib::placeholders::_1));
+		m_server.set_message_handler (bind (&websocket_server::on_message, this, websocketpp::lib::placeholders::_1, websocketpp::lib::placeholders::_2));
+	}
+
+	void on_open (connection_hdl hdl) {
+		m_connections.insert (hdl);
+	}
+
+	void on_close (connection_hdl hdl) {
+		m_connections.erase (hdl);
+	}
+
+	void on_message (connection_hdl hdl, server::message_ptr msg) {
+		for (auto it : m_connections) {
+			m_server.send (it, msg);
+		}
+	}
+
+	void run (uint16_t port) {
+		m_server.listen (port);
+		m_server.start_accept ();
+		m_server.run ();
+	}
+
+private:
+	typedef std::set<connection_hdl, std::owner_less<connection_hdl>> con_list;
+
+	server m_server;
+	con_list m_connections;
+};
 
 int main () {
-	server print_server;
-
-	print_server.set_message_handler (&on_message);
-	print_server.set_access_channels (websocketpp::log::alevel::all);
-	print_server.set_error_channels (websocketpp::log::elevel::all);
-
-	print_server.init_asio ();
-	print_server.listen (9002);
-	print_server.start_accept ();
-
-	print_server.run ();
+	websocket_server server;
+	server.run (9002);
 }
