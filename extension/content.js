a2aa51c7c4f113b016e88ee8400d84777c9506a9
TheGreatRambler <TheGreatRambler>
2021-02-19 02:42:49 -0500

    Small changes

--- extension/content.js
+++ extension/content.js
@@ -1,4 +1,4 @@
-function polymerClone (element) {
+function polymerClone(element) {
 	var newElement = element.cloneNode(true);
 
 	// Copying polymer elements is difficult
@@ -26,7 +26,7 @@ document.onload = function () {
 		} else if (possibleContainers.length == 12) {
 			videosContainer = possibleContainers[8];
 		}
-		var templateVideo         = videosContainer.firstElementChild;
+		var templateVideo = videosContainer.firstElementChild;
 		var templateVerifiedBadge = document.getElementsByClassName("badge-style-type-verified")[0];
 
 		// TODO will obtain data later
@@ -57,32 +57,32 @@ document.onload = function () {
 		videosContainer.textContent = "";
 
 		videosToPresent.forEach(function (video) {
-			var newVideo = polymerClone (templateVideo);
-
-			videosContainer.appendChild(newVideo);
+			var newVideo = polymerClone(templateVideo);
 
 			newVideo.style.opacity = "0%";
 
-			setTimeout (function () {
+			videosContainer.appendChild(newVideo);
+
+			setTimeout(function () {
 				var thumbnail = "https://i.ytimg.com/vi/" + video.id + "/hqdefault.jpg";
 				// They're locked behind some sort of tracking key
 				var movingThumbnail = "https://i.ytimg.com/an_webp/" + video.id + "/mqdefault_6s.webp?sqp=&rs=";
-				var url             = "https://www.youtube.com/watch?v=" + video.id;
+				var url = "https://www.youtube.com/watch?v=" + video.id;
 
 				// TODO thumbnails out of view dont render when you scroll to them
-				newVideo.__data.data.videoId                                                                        = video.id;
-				newVideo.__data.data.thumbnail.thumbnails[0]                                                        = thumbnail;
-				newVideo.__data.data.thumbnail.thumbnails[1]                                                        = thumbnail;
-				newVideo.__data.data.thumbnail.thumbnails[2]                                                        = thumbnail;
-				newVideo.__data.data.title.runs[0].text                                                             = video.title;
+				newVideo.__data.data.videoId = video.id;
+				newVideo.__data.data.thumbnail.thumbnails[0] = thumbnail;
+				newVideo.__data.data.thumbnail.thumbnails[1] = thumbnail;
+				newVideo.__data.data.thumbnail.thumbnails[2] = thumbnail;
+				newVideo.__data.data.title.runs[0].text = video.title;
 				newVideo.__data.data.richThumbnail.movingThumbnailRenderer.movingThumbnailDetails.thumbnails[0].url = movingThumbnail;
-				newVideo.__data.data.navigationEndpoint.watchEndpoint.videoId                                       = video.id;
+				newVideo.__data.data.navigationEndpoint.watchEndpoint.videoId = video.id;
 				// TODO the video wont appear if this is set
-				//newVideo.__data.data.navigationEndpoint.watchEndpoint.commandMetadata.webCommandMetadata.url                                                                                                                             = "/watch?v=" + video.id;
-				newVideo.__data.data.thumbnailOverlays[1].thumbnailOverlayToggleButtonRenderer.toggledServiceEndpoint.playlistEditEndpoint.actions[0].removedVideoId                                                                     = video.id;
-				newVideo.__data.data.thumbnailOverlays[1].thumbnailOverlayToggleButtonRenderer.untoggledServiceEndpoint.playlistEditEndpoint.actions[0].addedVideoId                                                                     = video.id;
-				newVideo.__data.data.thumbnailOverlays[2].thumbnailOverlayToggleButtonRenderer.untoggledServiceEndpoint.signalServiceEndpoint.actions[0].addToPlaylistCommand.videoId                                                    = video.id;
-				newVideo.__data.data.thumbnailOverlays[2].thumbnailOverlayToggleButtonRenderer.untoggledServiceEndpoint.signalServiceEndpoint.actions[0].addToPlaylistCommand.videoIds                                                   = [video.id];
+				//newVideo.__data.data.navigationEndpoint.watchEndpoint.commandMetadata.webCommandMetadata.url = "/watch?v=" + video.id;
+				newVideo.__data.data.thumbnailOverlays[1].thumbnailOverlayToggleButtonRenderer.toggledServiceEndpoint.playlistEditEndpoint.actions[0].removedVideoId = video.id;
+				newVideo.__data.data.thumbnailOverlays[1].thumbnailOverlayToggleButtonRenderer.untoggledServiceEndpoint.playlistEditEndpoint.actions[0].addedVideoId = video.id;
+				newVideo.__data.data.thumbnailOverlays[2].thumbnailOverlayToggleButtonRenderer.untoggledServiceEndpoint.signalServiceEndpoint.actions[0].addToPlaylistCommand.videoId = video.id;
+				newVideo.__data.data.thumbnailOverlays[2].thumbnailOverlayToggleButtonRenderer.untoggledServiceEndpoint.signalServiceEndpoint.actions[0].addToPlaylistCommand.videoIds = [video.id];
 				newVideo.__data.data.thumbnailOverlays[2].thumbnailOverlayToggleButtonRenderer.untoggledServiceEndpoint.signalServiceEndpoint.actions[0].addToPlaylistCommand.onCreateListCommand.createPlaylistServiceEndpoint.videoIds = [video.id];
 
 				if (video.verified) {
@@ -99,17 +99,17 @@ document.onload = function () {
 					newVideo.__data.data.ownerBadges = [];
 				}
 
-				newVideo.firstElementChild.children[0].firstElementChild.href                                    = url;
-				newVideo.firstElementChild.children[0].firstElementChild.search                                  = "?v=" + video.id;
-				newVideo.firstElementChild.children[1].firstElementChild.children[0].children[1]["aria-label"]   = video.title;
+				newVideo.firstElementChild.children[0].firstElementChild.href = url;
+				newVideo.firstElementChild.children[0].firstElementChild.search = "?v=" + video.id;
+				newVideo.firstElementChild.children[1].firstElementChild.children[0].children[1]["aria-label"] = video.title;
 				newVideo.firstElementChild.children[0].firstElementChild.firstElementChild.firstElementChild.src = thumbnail;
 				newVideo.firstElementChild.children[0].firstElementChild.firstElementChild.classList.remove("empty")
-				newVideo.firstElementChild.children[1].firstElementChild.children[0].children[1].title                                                                                                           = video.title;
-				newVideo.firstElementChild.children[1].firstElementChild.children[0].children[1].innerHTML                                                                                                       = video.title;
-				newVideo.firstElementChild.children[1].firstElementChild.children[0].children[1].href                                                                                                            = url;
-				newVideo.firstElementChild.children[0].firstElementChild.children[1].firstElementChild.children[1].innerHTML                                                                                     = "\n  " + video.length + "\n";
+				newVideo.firstElementChild.children[1].firstElementChild.children[0].children[1].title = video.title;
+				newVideo.firstElementChild.children[1].firstElementChild.children[0].children[1].innerHTML = video.title;
+				newVideo.firstElementChild.children[1].firstElementChild.children[0].children[1].href = url;
+				newVideo.firstElementChild.children[0].firstElementChild.children[1].firstElementChild.children[1].innerHTML = "\n  " + video.length + "\n";
 				newVideo.firstElementChild.children[1].firstElementChild.children[1].firstElementChild.children[0].firstElementChild.children[0].firstElementChild.firstElementChild.firstElementChild.innerHTML = video.creator;
-				newVideo.firstElementChild.children[1].firstElementChild.children[1].firstElementChild.children[0].firstElementChild.children[0].firstElementChild.firstElementChild.firstElementChild.href      = video.creatorchannel;
+				newVideo.firstElementChild.children[1].firstElementChild.children[1].firstElementChild.children[0].firstElementChild.children[0].firstElementChild.firstElementChild.firstElementChild.href = video.creatorchannel;
 
 				newVideo.firstElementChild.children[1].firstElementChild.children[1].firstElementChild.firstElementChild.firstElementChild.children[0].children[1].firstElementChild.innerHTML = "\n      \n    " + video.creator + "\n  \n    ";
 
@@ -126,27 +126,34 @@ document.onload = function () {
 				}
 
 				newVideo.style.opacity = "100%";
-			}, 100);
+			}, 300);
 		});
 	}
 
+	if ((window.location.href === "https://www.youtube.com/feed/feed" || window.location.href === "http://www.youtube.com/feed/feed")) {
+		localStorage.setItem("amRedirectingToFeed", "1");
+		window.location.href = "/feed/subscriptions";
+	}
+
 	var sidebarButtonIsLoaded = false;
-	var looper                = setInterval (function () {
-        var sidebarContainer = document.getElementById("section-items");
+	var looper = setInterval(function () {
+		var sidebarContainer = document.getElementById("section-items");
 
-        if (sidebarContainer) {
-            clearInterval (looper);
-            sidebarButtonIsLoaded = true;
+		if (sidebarContainer) {
+			clearInterval(looper);
+			sidebarButtonIsLoaded = true;
 
-            var newFeedElement = polymerClone (sidebarContainer.firstElementChild);
+			var newFeedElement = polymerClone(sidebarContainer.firstElementChild);
 
-            sidebarContainer.insertBefore(newFeedElement, sidebarContainer.children[1]);
+			sidebarContainer.insertBefore(newFeedElement, sidebarContainer.children[1]);
 
-            newFeedElement.firstElementChild.title = "Feed";
-            newFeedElement.firstElementChild.href  = "/feed/feed"; // TODO
+			newFeedElement.firstElementChild.title = "Feed";
+			newFeedElement.firstElementChild.href = "/feed/feed";
+			newFeedElement.__data.data.formattedTitle.simpleText = "Feed";
+			newFeedElement.__data.data.navigationEndpoint.commandMetadata.webCommandMetadata.url = "/feed/feed";
 
-            newFeedElement.firstElementChild.firstElementChild.children[2].innerHTML                         = "Feed";
-            newFeedElement.firstElementChild.firstElementChild.firstElementChild.firstElementChild.outerHTML = `
+			newFeedElement.firstElementChild.firstElementChild.children[2].innerHTML = "Feed";
+			newFeedElement.firstElementChild.firstElementChild.firstElementChild.firstElementChild.outerHTML = `
 			<svg viewBox="0 0 200 200" preserveAspectRatio="xMidYMid meet" focusable="false" style="pointer-events: none; display: block; width: 100%; height: 100%;" class="style-scope yt-icon" height="100%" width="100%" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" x="0px" y="0px" enable-background="new 0 0 200 200" xml:space="preserve">
 				<rect x="82.833" y="31.855" width="67.469" height="6.134"></rect>
 				<rect x="82.833" y="44.123" width="21.468" height="6.134"></rect>
@@ -157,11 +164,6 @@ document.onload = function () {
 				<path d="M168.429,7H30.887C24.559,7,19,12.452,19,18.783v160.461C19,185.575,24.559,191,30.887,191h137.542  c6.327,0,11.571-5.425,11.571-11.756V18.783C180,12.452,174.756,7,168.429,7z M168,19v44H31V19H168z M83,95v-6h67v6H83z M104,101v6  H83v-6H104z M67,89v21H46V89H67z M31,179v-46h137v46H31z"></path>
 			</svg>
 				`;
-
-            newFeedElement.onclick = function () {
-                localStorage.setItem("amRedirectingToFeed", "1");
-                window.location.href = "/feed/subscriptions";
-            };
-        }
-    }, 30);
+		}
+	}, 30);
 };
