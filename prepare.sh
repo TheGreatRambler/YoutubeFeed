3320b5acde917c26732980e1b51f310d90d76f36
TheGreatRambler <TheGreatRambler>
2021-02-21 02:51:59 -0500

    Fix some mongocxx building problems

--- prepare.sh
+++ prepare.sh
@@ -1,7 +1,7 @@
 cd server/lib/mongocdriver
 mkdir cmake-build
 cd cmake-build
-cmake -G "Unix Makefiles" .. -DCMAKE_BUILD_TYPE=Release -DENABLE_AUTOMATIC_INIT_AND_CLEANUP=OFF -DENABLE_BSON=ON -DENABLE_TESTS=0 -DENABLE_EXAMPLES=0 -DCMAKE_PREFIX_PATH=./cmake_build -DCMAKE_INSTALL_PREFIX=./cmake_build
+cmake -G "Unix Makefiles" .. -DCMAKE_BUILD_TYPE=Release -DENABLE_AUTOMATIC_INIT_AND_CLEANUP=OFF -DENABLE_BSON=ON -DENABLE_TESTS=0 -DENABLE_EXAMPLES=0 -DCMAKE_PREFIX_PATH=./cmake_build -DCMAKE_INSTALL_PREFIX=./cmake_build -DBUILD_SHARED_LIBS=OFF -DBUILD_SHARED_AND_STATIC_LIBS=OFF
 make
 make install
 rootFolder=$(pwd)
@@ -12,7 +12,7 @@ mkdir build
 cd build
 echo $rootFolder
 #cmake -G "Unix Makefiles" .. -DCMAKE_BUILD_TYPE=Release -Dlibbson-1.0_DIR="..\..\mongocdriver\cmake-build\cmake_build\lib\cmake\libbson-1.0"
-cmake -G "Unix Makefiles" .. -DCMAKE_BUILD_TYPE=Release -Dlibbson-1.0_DIR="${rootFolder}\cmake_build\lib\cmake\libbson-1.0" -Dlibmongoc-1.0_DIR="${rootFolder}\cmake_build\lib\cmake\libmongoc-1.0" -DCMAKE_CXX_FLAGS="-I'${rootFolder}\cmake_build\include\libmongoc-1.0' -I'${rootFolder}\cmake_build\include\libbson-1.0'"
+cmake -G "Unix Makefiles" .. -DCMAKE_BUILD_TYPE=Release -Dlibbson-static-1.0_DIR="${rootFolder}/cmake_build/lib/cmake/libbson-static-1.0" -Dlibmongoc-static-1.0_DIR="${rootFolder}/cmake_build/lib/cmake/libmongoc-static-1.0" -DCMAKE_CXX_FLAGS="-I'${rootFolder}/cmake_build/include/libmongoc-1.0' -I'${rootFolder}/cmake_build/include/libbson-1.0'" -DBUILD_SHARED_LIBS=OFF -DBUILD_SHARED_AND_STATIC_LIBS=OFF -DBSONCXX_POLY_USE_MNMLSTC=1
 make
 make install
 cd ../../../../
