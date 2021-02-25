0db40cdd1c772dbe2e7b7913718f85df51ff7b89
TheGreatRambler <aehart684351@gmail.com>
2021-02-24 22:00:12 -0500

    Add watch later listening

--- extension/content.js
+++ extension/content.js
@@ -1,7 +1,7 @@
-var ws = new WebSocket("ws://localhosttester.com:9002");
+var ws = new WebSocket ("ws://localhosttester.com:9002");
 var userChannelId;
 
-function polymerClone(element) {
+function polymerClone (element) {
 	var newElement = element.cloneNode(true);
 
 	// Copying polymer elements is difficult
@@ -12,7 +12,7 @@ function polymerClone(element) {
 	return newElement;
 }
 
-function sendChannelList() {
+function sendChannelList () {
 	document.getElementsByTagName("yt-guide-manager")[0].fetchGuideData().then(function (subscriptions) {
 		var channels = [];
 		//var startingContainer = document.getElementsByTagName("ytd-guide-section-renderer")[1];
@@ -28,6 +28,7 @@ function sendChannelList() {
 
 		ws.send(JSON.serialize({
 			flag: "SendSubscriptions",
+			userId: userChannelId,
 			data: channels
 		}));
 	});
@@ -98,40 +99,32 @@ document.onload = function () {
 		videosContainer.textContent = "";
 
 		videosToPresent.forEach(function (video) {
-			var newVideo = polymerClone(templateVideo);
+			var newVideo = polymerClone (templateVideo);
 
 			newVideo.style.opacity = "0%";
 
 			videosContainer.appendChild(newVideo);
 
-			setTimeout(function () {
+			setTimeout (function () {
 				var thumbnail = "https://i.ytimg.com/vi/" + video.id + "/hqdefault.jpg";
 				// They're locked behind some sort of tracking key
 				var movingThumbnail = "https://i.ytimg.com/an_webp/" + video.id + "/mqdefault_6s.webp?sqp=&rs=";
-				var url = "https://www.youtube.com/watch?v=" + video.id;
+				var url             = "https://www.youtube.com/watch?v=" + video.id;
 
 				// TODO thumbnails out of view dont render when you scroll to them
-				newVideo.__data.data.videoId = video.id;
-				newVideo.__data.data.thumbnail.thumbnails[0] = thumbnail;
-				newVideo.__data.data.thumbnail.thumbnails[1] = thumbnail;
-				newVideo.__data.data.thumbnail.thumbnails[2] = thumbnail;
-				newVideo.__data.data.title.runs[0].text = video.title;
+				newVideo.__data.data.videoId                                                                        = video.id;
+				newVideo.__data.data.thumbnail.thumbnails[0]                                                        = thumbnail;
+				newVideo.__data.data.thumbnail.thumbnails[1]                                                        = thumbnail;
+				newVideo.__data.data.thumbnail.thumbnails[2]                                                        = thumbnail;
+				newVideo.__data.data.title.runs[0].text                                                             = video.title;
 				newVideo.__data.data.richThumbnail.movingThumbnailRenderer.movingThumbnailDetails.thumbnails[0].url = movingThumbnail;
-				newVideo.__data.data.navigationEndpoint.watchEndpoint.videoId = video.id;
+				newVideo.__data.data.navigationEndpoint.watchEndpoint.videoId                                       = video.id;
 				// TODO the video wont appear if this is set
 				//newVideo.__data.data.navigationEndpoint.watchEndpoint.commandMetadata.webCommandMetadata.url = "/watch?v=" + video.id;
 				newVideo.__data.data.thumbnailOverlays[1].thumbnailOverlayToggleButtonRenderer.toggledServiceEndpoint.playlistEditEndpoint.actions[0].removedVideoId = video.id;
 
-				Object.defineProperty(newVideo.__data.data.thumbnailOverlays[1].thumbnailOverlayToggleButtonRenderer.untoggledServiceEndpoint.playlistEditEndpoint.actions[0], "addedVideoId", {
-					get: function () {
-						// Video added to watchlist
-						console.log("Added to watchlist");
-						return video.id;
-					}
-				});
-
-				newVideo.__data.data.thumbnailOverlays[2].thumbnailOverlayToggleButtonRenderer.untoggledServiceEndpoint.signalServiceEndpoint.actions[0].addToPlaylistCommand.videoId = video.id;
-				newVideo.__data.data.thumbnailOverlays[2].thumbnailOverlayToggleButtonRenderer.untoggledServiceEndpoint.signalServiceEndpoint.actions[0].addToPlaylistCommand.videoIds = [video.id];
+				newVideo.__data.data.thumbnailOverlays[2].thumbnailOverlayToggleButtonRenderer.untoggledServiceEndpoint.signalServiceEndpoint.actions[0].addToPlaylistCommand.videoId                                                    = video.id;
+				newVideo.__data.data.thumbnailOverlays[2].thumbnailOverlayToggleButtonRenderer.untoggledServiceEndpoint.signalServiceEndpoint.actions[0].addToPlaylistCommand.videoIds                                                   = [video.id];
 				newVideo.__data.data.thumbnailOverlays[2].thumbnailOverlayToggleButtonRenderer.untoggledServiceEndpoint.signalServiceEndpoint.actions[0].addToPlaylistCommand.onCreateListCommand.createPlaylistServiceEndpoint.videoIds = [video.id];
 
 				if (video.verified) {
@@ -148,17 +141,17 @@ document.onload = function () {
 					newVideo.__data.data.ownerBadges = [];
 				}
 
-				newVideo.firstElementChild.children[0].firstElementChild.href = url;
-				newVideo.firstElementChild.children[0].firstElementChild.search = "?v=" + video.id;
-				newVideo.firstElementChild.children[1].firstElementChild.children[0].children[1]["aria-label"] = video.title;
+				newVideo.firstElementChild.children[0].firstElementChild.href                                    = url;
+				newVideo.firstElementChild.children[0].firstElementChild.search                                  = "?v=" + video.id;
+				newVideo.firstElementChild.children[1].firstElementChild.children[0].children[1]["aria-label"]   = video.title;
 				newVideo.firstElementChild.children[0].firstElementChild.firstElementChild.firstElementChild.src = thumbnail;
 				newVideo.firstElementChild.children[0].firstElementChild.firstElementChild.classList.remove("empty")
-				newVideo.firstElementChild.children[1].firstElementChild.children[0].children[1].title = video.title;
-				newVideo.firstElementChild.children[1].firstElementChild.children[0].children[1].innerHTML = video.title;
-				newVideo.firstElementChild.children[1].firstElementChild.children[0].children[1].href = url;
-				newVideo.firstElementChild.children[0].firstElementChild.children[1].firstElementChild.children[1].innerHTML = "\n  " + video.length + "\n";
+				newVideo.firstElementChild.children[1].firstElementChild.children[0].children[1].title                                                                                                           = video.title;
+				newVideo.firstElementChild.children[1].firstElementChild.children[0].children[1].innerHTML                                                                                                       = video.title;
+				newVideo.firstElementChild.children[1].firstElementChild.children[0].children[1].href                                                                                                            = url;
+				newVideo.firstElementChild.children[0].firstElementChild.children[1].firstElementChild.children[1].innerHTML                                                                                     = "\n  " + video.length + "\n";
 				newVideo.firstElementChild.children[1].firstElementChild.children[1].firstElementChild.children[0].firstElementChild.children[0].firstElementChild.firstElementChild.firstElementChild.innerHTML = video.creator;
-				newVideo.firstElementChild.children[1].firstElementChild.children[1].firstElementChild.children[0].firstElementChild.children[0].firstElementChild.firstElementChild.firstElementChild.href = video.creatorChannel;
+				newVideo.firstElementChild.children[1].firstElementChild.children[1].firstElementChild.children[0].firstElementChild.children[0].firstElementChild.firstElementChild.firstElementChild.href      = video.creatorChannel;
 
 				newVideo.firstElementChild.children[1].firstElementChild.children[1].firstElementChild.firstElementChild.firstElementChild.children[0].children[1].firstElementChild.innerHTML = "\n      \n    " + video.creator + "\n  \n    ";
 
@@ -186,7 +179,7 @@ document.onload = function () {
 
 	if (window.location.href.includes("youtube.com/watch?")) {
 		// Listen for watch, like or watchlist
-		var likeButton = document.getElementsByTagName("ytd-toggle-button-renderer")[0];
+		var likeButton     = document.getElementsByTagName("ytd-toggle-button-renderer")[0];
 		likeButton.onclick = function () {
 			if (likeButton.__data.data.isToggled) {
 				// Video was liked
@@ -194,7 +187,8 @@ document.onload = function () {
 			}
 		}
 
-		var watchlistCheckbox = document.getElementsByTagName("tp-yt-paper-checkbox")[0];
+		var watchlistCheckbox
+			= document.getElementsByTagName("tp-yt-paper-checkbox")[0];
 		watchlistCheckbox.onclick = function () {
 			if (watchlistCheckbox.__data.checked) {
 				// Video was added to watchlist
@@ -204,7 +198,7 @@ document.onload = function () {
 	}
 
 	if (window.location.href.includes("youtube.com/channel")) {
-		var possibleButtons = document.getElementsByTagName("ytd-subscribe-button-renderer");
+		var possibleButtons          = document.getElementsByTagName("ytd-subscribe-button-renderer");
 		var subscribeButtonContainer = possibleButtons[possibleButtons.length - 1];
 
 		var newSubscribeButton = document.createElement("ytd-subscribe-button-renderer");
@@ -213,8 +207,8 @@ document.onload = function () {
 
 		var alreadyFollowed = true;
 
-		newSubscribeButton.__data.buttonAccessibility = "Follow this channel";
-		newSubscribeButton.firstElementChild.firstElementChild.innerText = "FOLLOW";
+		newSubscribeButton.__data.buttonAccessibility                      = "Follow this channel";
+		newSubscribeButton.firstElementChild.firstElementChild.innerText   = "FOLLOW";
 		newSubscribeButton.__data.data.unsubscribedButtonText.runs[0].text = "Unfollow";
 		newSubscribeButton.__data.data.unsubscribedButtonText.runs[0].text = "";
 
@@ -224,24 +218,24 @@ document.onload = function () {
 	if (window.location.href.includes("youtube.com")) {
 		// Sidebar
 		var sidebarButtonIsLoaded = false;
-		var looper = setInterval(function () {
-			var sidebarContainer = document.getElementById("section-items");
+		var looper                = setInterval (function () {
+            var sidebarContainer = document.getElementById("section-items");
 
-			if (sidebarContainer) {
-				clearInterval(looper);
-				sidebarButtonIsLoaded = true;
+            if (sidebarContainer) {
+                clearInterval (looper);
+                sidebarButtonIsLoaded = true;
 
-				var newFeedElement = polymerClone(sidebarContainer.firstElementChild);
+                var newFeedElement = polymerClone (sidebarContainer.firstElementChild);
 
-				sidebarContainer.insertBefore(newFeedElement, sidebarContainer.children[1]);
+                sidebarContainer.insertBefore(newFeedElement, sidebarContainer.children[1]);
 
-				newFeedElement.firstElementChild.title = "Feed";
-				newFeedElement.firstElementChild.href = "/feed/feed";
-				newFeedElement.__data.data.formattedTitle.simpleText = "Feed";
-				newFeedElement.__data.data.navigationEndpoint.commandMetadata.webCommandMetadata.url = "/feed/feed";
+                newFeedElement.firstElementChild.title                                               = "Feed";
+                newFeedElement.firstElementChild.href                                                = "/feed/feed";
+                newFeedElement.__data.data.formattedTitle.simpleText                                 = "Feed";
+                newFeedElement.__data.data.navigationEndpoint.commandMetadata.webCommandMetadata.url = "/feed/feed";
 
-				newFeedElement.firstElementChild.firstElementChild.children[2].innerHTML = "Feed";
-				newFeedElement.firstElementChild.firstElementChild.firstElementChild.firstElementChild.outerHTML = `
+                newFeedElement.firstElementChild.firstElementChild.children[2].innerHTML                         = "Feed";
+                newFeedElement.firstElementChild.firstElementChild.firstElementChild.firstElementChild.outerHTML = `
 				<svg viewBox="0 0 200 200" preserveAspectRatio="xMidYMid meet" focusable="false" style="pointer-events: none; display: block; width: 100%; height: 100%;" class="style-scope yt-icon" height="100%" width="100%" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" x="0px" y="0px" enable-background="new 0 0 200 200" xml:space="preserve">
 					<rect x="82.833" y="31.855" width="67.469" height="6.134"></rect>
 					<rect x="82.833" y="44.123" width="21.468" height="6.134"></rect>
@@ -252,21 +246,41 @@ document.onload = function () {
 					<path d="M168.429,7H30.887C24.559,7,19,12.452,19,18.783v160.461C19,185.575,24.559,191,30.887,191h137.542  c6.327,0,11.571-5.425,11.571-11.756V18.783C180,12.452,174.756,7,168.429,7z M168,19v44H31V19H168z M83,95v-6h67v6H83z M104,101v6  H83v-6H104z M67,89v21H46V89H67z M31,179v-46h137v46H31z"></path>
 				</svg>
 					`;
-			}
-		}, 30);
-
-		// Always send channel list on boot
-		sendChannelList();
+            }
+        }, 30);
 
 		// Obtain this user's channel ID
 		document.getElementsByTagName("ytd-popup-container")[0].style.opacity = "0%";
 		document.getElementsByTagName("ytd-topbar-menu-button-renderer")[2].onTap();
-		setTimeout(function () {
+		setTimeout (function () {
 			userChannelId = document.getElementsByTagName("ytd-popup-container")[0].firstElementChild.firstElementChild.firstElementChild.children[2].firstElementChild.firstElementChild.children[1].firstElementChild.__data.data.navigationEndpoint.browseEndpoint.browseId;
-			setTimeout(function () {
+
+			// Always send channel list on boot
+			sendChannelList ();
+
+			setTimeout (function () {
 				document.getElementsByTagName("ytd-topbar-menu-button-renderer")[2].onTap();
 				document.getElementsByTagName("ytd-popup-container")[0].style.opacity = "100%";
 			}, 500);
 		}, 500);
+
+		// Notice every watchlist modification
+		setInterval (function () {
+			Array.from(document.getElementsByTagName("ytd-thumbnail-overlay-toggle-button-renderer")).forEach(function (item) {
+				if (item.__data.data.untoggledTooltip == "Watch later") {
+					var id = item.__data.data.toggledServiceEndpoint.playlistEditEndpoint.actions[0].removedVideoId;
+
+					console.log(id);
+
+					item.children[1].onclick = function () {
+						if (item.__data.toggled) {
+							console.log("ID " + id + " is enabled");
+						} else {
+							console.log("ID " + id + " is disabled");
+						}
+					}
+				}
+			})
+		}, 50);
 	}
 };
