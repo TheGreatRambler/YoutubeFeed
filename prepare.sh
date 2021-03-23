345a79e80f5555b676875466c8dc9e6d5befc9bb
TheGreatRambler <aehart684351@gmail.com>
2021-03-22 22:40:52 -0400

    Add other required packages when preparing

--- prepare.sh
+++ prepare.sh
@@ -18,9 +18,14 @@ make install
 cd ../../../../
 
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-    sudo apt-get install libssl-dev
-    sudo apt-get install libfmt-dev
+    sudo apt-get install -y libssl-dev
+    sudo apt-get install -y libfmt-dev
+    sudo apt-get install -y zlib1g-dev
+    sudo apt-get install -y zstd-dev
+    sudo apt-get install -y libicu-dev
 else
     pacman -S mingw-w64-x86_64-openssl
     pacman -S mingw-w64-x86_64-fmt
+    pacman -S mingw-w64-x86_64-zlib
+    pacman -S mingw-w64-x86_64-zstd
 fi
